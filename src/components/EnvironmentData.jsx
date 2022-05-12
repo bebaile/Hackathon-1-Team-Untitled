baffb539761233a99fc8781e830489f94a5f8a9d
Basile Carle <basile.carle@edhec.com>
2022-05-12 17:59:45 +0200

    ajout page Home et integration composant maps et environmentdata

---
+++ src/components/EnvironmentData.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EnvironmentData = () => {
+  return <div />;
+};
+
+export default EnvironmentData;
---
+++ src/components/Maps.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Maps = () => {
+  return <div />;
+};
+
+export default Maps;
