f306b56d3ceaacb876bf7470f43b4ec95c075423
froustex <stephane-feroussier_student2022@wilder.school>
2022-05-12 23:33:11 +0200

    premier commit Connexion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Home from "./pages/Home";
 import Connexion from "./pages/Connexion";
 import LandingPage from "./pages/LandingPage";
@@ -6,7 +6,10 @@ import { Routes, Route } from "react-router-dom";
 import { MainContextProvider } from "./contexts/MainContext";
 
 const App = () => {
-  //const [isOnline, setIsOnline] = useState(false);
+  const [isOnline, setIsOnline] = useState();
+  if(!isOnline){
+    return <Connexion setIsOnline={setIsOnline}/>
+  }
 
   // a mettre dans les composants enfants
   //const { isOnline, setIsOnline } = useContext(MainContext);
--- src/pages/Connexion.jsx
+++ src/pages/Connexion.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React from "react"; 
 
-const Connexion = () => {
-  return <div> coucou</div>;
-};
+  const database = [
+    {
+      username: "Olivier",
+      password: "Pochic"
+    },
+    {
+      username: "Ali",
+      password: "Belala"
+    },
+    {
+      username: "Basile",
+      password: "Carle"
+    },
+    {
+      username: "Audren",
+      password: "Floch"
+    },
+    {
+      username: "Stephane",
+      password: "Feroussier"
+    }
+  ];
 
-export default Connexion;
+export default function Login() {
+  return(
+  <div className="login-wrapper">
+    <h1>Veuillez vous connecter</h1>
+    <form>
+      <label>
+        <p>Nom Utilisateur</p>
+        <input type="text" />
+      </label>
+      <label>
+        <p>Mot de Passe</p>
+        <input type="password" />
+      </label>
+      <div>
+        <button type="submit">Soumettre</button>
+      </div>
+    </form>
+  </div> 
+  )
+}
