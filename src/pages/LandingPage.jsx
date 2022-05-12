a7a37a0e13249d024b732737837109aa93e230c2
ALI BELALA <alouch74160@gmail.com>
2022-05-13 00:09:08 +0200

    création des composants LandingPage.jsx et LandingPage.css

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,42 @@
-import React /*, { useState, useContext }*/ from "react";
+import React , { useState, useContext } from "react";
 //import { MainContext } from "../contexts/MainContext";
 //const { isOnline, setIsOnline } = useContext(MainContext);
+import { Link } from "react-router-dom";
+import logo from "./assets/logo.jpg";
+
 
 const LandingPage = () => {
-  return <div />;
+  const [isLogging, setIsLogging] = useState(false)
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setIsLogging(!isLogging)
+  };
+
+  return (
+    <>
+    <div className="logo">
+        <img src={logo} alt="logo" className="img-logo" />
+    </div>
+    <div className = "About">
+      <h1> À propos</h1>
+       <p>Ce site à été crée afin de sensibiliser la population à l'éco-responsabilité. 
+         <br />
+         Ce site vous permettra d'en savoir un peu plus sur la qualité de l'air et de l'eau près de chez vous (où dans une zone que vous aurez choisi) et ainsi agir en conséquence de manière éco-responsable.
+         <br />
+         En créant votre compte vous pourrez facilement retrouver les endroits ciblés auparavant en les mettant en favoris.
+       </p>
+    </div>
+    <div className = "Sign-in">
+      <Link to = "/Connexion">
+        <button onClick = {handleClick}
+            type="button"
+            placeholder="Connecte-toi"  
+          />
+      </Link>  
+    </div>
+      </>
+     )
 };
 
 export default LandingPage;
