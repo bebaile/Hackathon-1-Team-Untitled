5021c51c85de8a7648370545af7fb1aa40b14a0e
AUDREN FLOCH <a.floch@outlook.fr>
2022-05-13 11:27:28 +0200

    ;)

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,55 @@
-import React /*, { useState, useContext }*/ from "react";
+import React, { useState, useContext } from "react";
 //import { MainContext } from "../contexts/MainContext";
 //const { isOnline, setIsOnline } = useContext(MainContext);
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.jpg";
 
 const LandingPage = () => {
-  return <div />;
+  const [isLogging, setIsLogging] = useState(false);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setIsLogging(!isLogging);
+  };
+
+  return (
+    <>
+      <div className="logo">
+        <img src={logo} alt="logo" className="img-logo" />
+      </div>
+      {/* <div className="About-us">
+        <h1> Qui sommes nous?</h1>
+          <p> Nous sommes Ali, Audren, Basile, Olivier et Stéphane, une équipe de 5 juniors développeurs </p>
+      </div> */}
+      <div className="About">
+        <h1> À propos</h1>
+        <p>
+          Ce site à été crée afin de sensibiliser la population à
+          l'éco-responsabilité.
+          <br />
+          <br />
+          Ce site vous permettra d'en savoir un peu plus sur la qualité de l'air
+          et de l'eau près de chez vous (où dans une zone que vous aurez choisi)
+          et ainsi d'agir en conséquence de manière éco-responsable.
+          <br />
+          <br />
+          En créant votre compte vous pourrez facilement retrouver les endroits
+          ciblés auparavant en les mettant en favoris.
+        </p>
+      </div>
+      <div className="Sign-in">
+        <Link to="/Connexion">
+          <button
+            onClick={handleClick}
+            type="button"
+            placeholder="Connecte-toi"
+          >
+            Connecte toi{" "}
+          </button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default LandingPage;
