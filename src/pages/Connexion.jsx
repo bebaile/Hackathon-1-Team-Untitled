6c7c9abd98bd18062b6dd050fee7cdccb80cd374
AUDREN FLOCH <a.floch@outlook.fr>
2022-05-13 11:20:04 +0200

    c tro bo

--- src/pages/Connexion.jsx
+++ src/pages/Connexion.jsx
@@ -1,27 +1,27 @@
-import React from "react"; 
+import React from "react";
 
- /* const database = [
-    {
-      username: "Olivier",
-      password: "Pochic"
-    },
-    {
-      username: "Ali",
-      password: "Belala"
-    },
-    {
-      username: "Basile",
-      password: "Carle"
-    },
-    {
-      username: "Audren",
-      password: "Floch"
-    },
-    {
-      username: "Stephane",
-      password: "Feroussier"
-    }
-  ];*/
+/* const database = [
+   {
+     username: "Olivier",
+     password: "Pochic"
+   },
+   {
+     username: "Ali",
+     password: "Belala"
+   },
+   {
+     username: "Basile",
+     password: "Carle"
+   },
+   {
+     username: "Audren",
+     password: "Floch"
+   },
+   {
+     username: "Stephane",
+     password: "Feroussier"
+   }
+ ];*/
 
 
 const handleSubmit = (event) => {
@@ -31,24 +31,30 @@ const handleSubmit = (event) => {
 
 export default function Login() {
 
-  return(
-  <div className="container">
-    <div className="login-wrapper">
-      <h1>Veuillez vous connecter</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p className="info">Nom Utilisateur</p>
-          <input className="input" type="text" name="" />
-        </label>
-        <label>
-          <p className="info">Mot de Passe</p>
-          <input className="input"  type="password"/>
-        </label>
-        <div>
-          <button type="submit" onSubmit={handleSubmit}>Soumettre</button>
-        </div>
-      </form>
-    </div>  
-  </div>
+  return (
+    <div className="container">
+      <div className="login-wrapper">
+        <h1>Veuillez vous connecter</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form_body">
+            <div className="body_form">
+              <label className="info">Nom Utilisateur :</label>
+              <input className="input" type="text" name="" />
+            </div>
+            <div className="body_form">
+              <label className="info">Mot de Passe :</label>
+              <input className="input" type="password" />
+            </div>
+            <div>
+              <label className="body_form">Votre message :</label>
+              <textarea id="message" name="message" placeholder="Parlez-nous un peu de vos motivations écologiques..."></textarea>
+            </div>
+          </div>
+          <div className="sb">
+            <button className="submit_button" type="submit" onSubmit={handleSubmit}>Soumettre</button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
